c197e83debd36003d9c42bd9afeb9203dda23a09
STAKXX002 <203787821+STAKXX002@users.noreply.github.com>
2026-09-15 13:41:09 +0530

    feat: add hatch_stop function to cut H-bridge power on fault

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -210,6 +210,7 @@ void fault(const char *msg) {
     axis_stop(&z1);
     axis_stop(&z2);
     disable_motors();
+    hatch_stop(); // ADDED: Cut H-bridge power on fault
     calibrated = false;
     state = FAULT;
     printf("FAULT: %s\r\n", msg);
